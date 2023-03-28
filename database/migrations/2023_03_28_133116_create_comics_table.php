5beb9cad65f2aea2b789a3ce10092f07dd8da0c8
Spectrum5 <toskusergio@gmail.com>
2023-03-28 15:40:25 +0200

    Milestone 0 completata

---
+++ database/migrations/2023_03_28_133116_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('slug', 128)->unique();
+            $table->text('description', 4096);
+            $table->string('image')->nullable();
+            $table->float('price', 6,2);
+            $table->unsignedTinyInteger('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
