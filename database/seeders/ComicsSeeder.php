2498a20b0efb2ada48f6ed0e5ed63bc76ad5ee50
Spectrum5 <toskusergio@gmail.com>
2023-03-28 17:20:41 +0200

    Aggiunto Seeder

--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -5,7 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ComicsSeeder extends Seeder
+// Model
+use App\Models\Comic;
+
+// Helpers
+use Faker\Factory as FakerFactory;
+use Illuminate\Support\Str;
+
+
+class ComicSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,6 +22,24 @@ class ComicsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = FakerFactory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $title = $faker->unique()->sentence(4);
+            $slug = Str::slug($title);
+            $description = $faker->paragraph();
+            $image = $faker->imageUrl(640, 480, 'comics');
+            $price = $faker->randomFloat(2, 5, 50);
+            $quantity = $faker->numberBetween(0, 100);
+
+            Comic::create([
+                'name' => $title,
+                'slug' => $slug,
+                'description' => $description,
+                'image' => $image,
+                'price' => $price,
+                'quantity' => $quantity,
+            ]);
+        }
     }
 }
