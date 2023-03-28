76eaf2dc33a875600029576beb4c7c44ffd95471
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-28 16:10:32 +0200

    Add creazione branch

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+
+// controller
+use App\Http\Controllers\Controller;
 
 use App\Models\comics;
 use App\Http\Requests\StorecomicsRequest;
